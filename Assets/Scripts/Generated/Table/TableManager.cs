3cac178d690bc37fd4494a85133f47ed1c115d03
DESKTOP-V53H2DQ\cksdu <cksdud624@naver.com>
2026-08-25 15:42:30 +0900

    병합 시 테이블 컬럼 변경 사항 적용

--- Assets/Scripts/Generated/Table/TableManager.cs
+++ Assets/Scripts/Generated/Table/TableManager.cs
@@ -6,20 +6,20 @@ namespace Generated.Table
 	public class TableManager : MonoBehaviour
 	{
 		public CharacterRecord CharacterRecord {get; private set;}
-		public ObjectRecord ObjectRecord {get; private set;}
 		public AttackCommandRecord AttackCommandRecord {get; private set;}
 		public AttackHitBoxRecord AttackHitBoxRecord {get; private set;}
+		public ObjectRecord ObjectRecord {get; private set;}
 
 		public async UniTask Init()
 		{
 			CharacterRecord = new ();
 			await CharacterRecord.Init();
-			ObjectRecord = new ();
-			await ObjectRecord.Init();
 			AttackCommandRecord = new ();
 			await AttackCommandRecord.Init();
 			AttackHitBoxRecord = new ();
 			await AttackHitBoxRecord.Init();
+			ObjectRecord = new ();
+			await ObjectRecord.Init();
 		}
 	}
 }
